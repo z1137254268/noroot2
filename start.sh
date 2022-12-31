1b2350367d6cc3e2c9c87b90fde75559e707f0f8
z1137254268 <51695259+z1137254268@users.noreply.github.com>
2023-01-01 05:22:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,6 @@
 AUUID=10974d1a-cbd6-4b6f-db1d-38d78b3fb109
 PORT=${PORT:-8080}
 
-cat etc/Caddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(./caddy hash-password --plaintext $AUUID)/g" > etc/caddy/Caddyfile
-cat etc/config.json | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" > xray.json
-
 
 ./xray -config xray.json &
 ./caddy run --config etc/caddy/Caddyfile --adapter caddyfile
