fe5ffb0c9acad46c776074fe6c1ffd4298fe22f8
z1137254268 <51695259+z1137254268@users.noreply.github.com>
2023-01-01 05:09:40 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-# set val
-PORT=${PORT:-8080}
-AUUID=${AUUID:-5194845a-cacf-4515-8ea5-fa13a91b1026}
-ParameterSSENCYPT=${ParameterSSENCYPT:-chacha20-ietf-poly1305}
 
-chmod +x caddy xray p2pclient
-
-# set config file
-cat etc/Caddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(./caddy hash-password --plaintext $AUUID)/g" > etc/caddy/Caddyfile
-cat etc/config.json | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" > xray.json
-
-
-# start service
 ./xray -config xray.json &
-#./p2pclient -l 1137254268@qq.com &
-./caddy run --config etc/caddy/Caddyfile --adapter caddyfile
+
